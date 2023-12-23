d4941f61c5f525ca941000a813e4e3c260d61da6
felix <feliquisds@gmail.com>
2023-12-22 21:58:03 -0300

    fix: conclui sistema

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -4,20 +4,17 @@ namespace TestesUnitarios.Desafio.Console.Services
     {
         public List<int> RemoverNumerosNegativos(List<int> lista)
         {
-            var listaSemNegativos = lista.Where(x => x > 0);
-            return listaSemNegativos.ToList();
+            return lista.Where(x => x > 0).ToList();
         }
 
         public bool ListaContemDeterminadoNumero(List<int> lista, int numero)
         {
-            var contem = lista.Contains(numero);
-            return contem;
+            return lista.Contains(numero);
         }
 
         public List<int> MultiplicarNumerosLista(List<int> lista, int numero)
         {
-            var listaMultiplicada = lista.Select(x => x * numero).ToList();
-            return listaMultiplicada;
+            return lista.Select(x => x * numero).ToList();
         }
 
         public int RetornarMaiorNumeroLista(List<int> lista)
